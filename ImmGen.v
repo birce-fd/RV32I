3d5cfd7f66db8783cafc91e7b591b4aa68fc276e
ahmetyilmas <ahmetyilmas03@gmail.com>
2024-10-10 16:45:52 +0300

    Signed immediate için düzeltme

--- ImmGen.v
+++ ImmGen.v
@@ -20,25 +20,30 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module ImmGen(OP, Instr, outImm);
-    input [6:0]OP;
-    input [31:7]Instr;
+module ImmGen(Instr, outImm);
+    input [31:0]Instr;
     output [31:0] outImm;
     
-    assign outImm = select(OP);
+    assign outImm = select(Instr[31:0]);
     
     function [31:0]select;
-        input [6:0]OP;
+        input [31:0]Instr;
         
-        case (OP)
-            7'b0110111 : select = {12'b0, Instr[31:12]}; //LUI
-            7'b0010111 : select = {12'b0, Instr[31:12]}; //AUIPC
-            7'b1101111 : select = {12'b0, Instr[31], Instr[19:12], Instr[20], Instr[30:21]}; //JAL
-            7'b1100111 : select = {20'b0, Instr[31:20]}; //JALR
-            7'b1100011 : select = {20'b0, Instr[31], Instr[7], Instr[30:25], Instr[11:8]};  //BEQ, BNE, BLT, BGE, BLTU, BGEU 
-            7'b0000011 : select = {20'b0, Instr[31:20]}; // LB, LH, LW, LBU, LHU
-            7'b0100011 : select = {20'b0, Instr[31:25], Instr[11:7]};   //SB, SH, SW
-            7'b0010011 : select = {20'b0, Instr[31:20]}; //  ADDI, SLTI, SLTIU, XORI, ORI
+        case (Instr[6:0]) 
+        
+            //  I - tip
+            7'b0000011, 7'b0010011 : select = {{20{Instr[31]}}, Instr[31:20]};
+            //  S - tip
+            7'b0100011: select = {{20{Instr[31]}}, Instr[31:25], Instr[11:7]};
+            //  B - tip
+             7'b1100011: select = {{19{Instr[31]}}, Instr[31], Instr[7], Instr[30:25], Instr[11:8], 1'b0};
+             // U - tip
+             7'b0110111, 7'b0010111 : select = {Instr[31:12], 12'b0};
+             // J - tip
+             7'b1101111: select = {{11{Instr[31]}}, Instr[31], Instr[19:12], Instr[20], Instr[30:21], 1'b0};
+             
+            default : select = 32'b0;
+            
         endcase
     endfunction
         
