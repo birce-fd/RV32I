238ccf117ee3f7a2a2a3c546752cad9b37f7a978
ahmetyilmas <ahmetyilmas03@gmail.com>
2024-10-11 15:30:06 +0300

    JAL ve JALR eklendi

--- branch_condition.v
+++ branch_condition.v
@@ -27,21 +27,28 @@ module branch_condition(branch, LessFlag, ZeroFlag, PCAsrc, PCBsrc);
     
     
     /*
+    
     BEQ   000
     BNE   001
     BLT   010
     BGE   011
     BLTU  010
     BGEU  011
+    JAL   100
+    JALR  101
     NO BR 111
     */
-
     //  PCA = 0 -> imm, PCA = 1 -> 4
     //  PCB = 0 -> PC,  PCB = 1 -> rs1
     
     //  PCArsc ve PCBsrc 2x1 mux girişleri için select ucu olacak. Eger branch condition saglanmamis ise
-    //  PCAsrc = 1 ve PCBsrc = 0 (PC + 4) olacagi icin default olarak PCA = 1 ve PCB = 0 
+    //  PCAsrc = 1 ve PCBsrc = 0 (PC + 4)olacagi icin default olarak PCA = 1 ve PCB = 0 
     //  olsun, kosul saglanmis ise PCAsrc = 0 olsun (PC + offset).
+    
+    
+    //  Kosulsuz sekilde JAL ve JALR buruklarini calistirabilmek icin ZerFlag ve LEssFlag umursanmadan
+    //  sadece gelen buyruk koduna gore islem yapılır
+    
     
     initial begin
         PCAsrc = 1;
@@ -59,6 +66,13 @@ module branch_condition(branch, LessFlag, ZeroFlag, PCAsrc, PCBsrc);
             3'b010 : PCAsrc = (LessFlag == 1) ? 0 : 1;
             //  BGE BGEU
             3'b011 : PCAsrc = (LessFlag != 1) ? 0 : 1;
+            //  JAL     PC + offset -> PCA = 0, PCB = 0
+            3'b100 : PCAsrc = 0;
+            //  JALR    rs1 + offset -> PCA = 0, PCB = 1
+            3'b101 : begin
+                    PCAsrc = 0;
+                    PCBsrc = 1;
+                    end
             //  NO BRANCH
             3'b111 : PCAsrc = 1;
             default : PCAsrc = 1;
